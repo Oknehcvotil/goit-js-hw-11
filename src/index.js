8e65a5aafdc7f390ba7ef93d019d0fc592d8ba53
Kirill <litovchenko.br@gmail.com>
2023-04-23 02:21:52 +0300

    hw
    finished all main tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import InfiniteScroll from 'infinite-scroll';
+const axios = require('axios').default;
 
 const refs = {
   form: document.querySelector('#search-form'),
@@ -7,9 +11,11 @@ const refs = {
   loadBtn: document.querySelector('button[type="button"]'),
   gallery: document.querySelector('.gallery'),
 };
+
 const API_KEY = '35664571-75b29dbb0058a8cd226bd52d4';
 const URL = 'https://pixabay.com/api/';
 let page = 1;
+let currentHits = null;
 let searchValue = '';
 
 refs.form.addEventListener('submit', onSubmit);
@@ -17,73 +23,97 @@ refs.loadBtn.addEventListener('click', onLoad);
 
 function onSubmit(e) {
   e.preventDefault();
+  refs.gallery.innerHTML = '';
   searchValue = refs.input.value.trim();
+  if (!searchValue) {
+    return;
+  }
+  page = 1;
+  getImgs(searchValue);
+  const lightbox = new SimpleLightbox('.photo-card a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 
-  fetch(
-    `${URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-  )
-    .then(r => {
-      if (!r.ok) {
-        return;
-      }
-      return r.json();
-    })
-    .then(data => {
-      page += 1;
-      console.log(data);
-      if (data.totalHits === 0) {
-        return Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      renderSearchCard(data.hits);
-    })
-    .catch(() =>
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      )
+async function getImgs(searchValue) {
+  try {
+    const response = await axios.get(
+      `${URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
     );
+    const { data } = response;
+
+    if (data.totalHits === 0) {
+      return error;
+    }
+
+    refs.loadBtn.classList.add('visible');
+
+    if (page === 1) {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      currentHits = null;
+    }
+
+    currentHits += data.hits.length;
+    console.log(currentHits);
+
+    renderSearchCard(data.hits);
+
+    openModal();
+
+    if (currentHits >= data.totalHits) {
+      currentHits = null;
+      page = 1;
+      refs.loadBtn.classList.remove('visible');
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    console.log(error);
+    refs.loadBtn.classList.remove('visible');
+    refs.gallery.innerHTML = '';
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
 }
 
 function onLoad(e) {
-  fetch(
-    `${URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-  )
-    .then(r => {
-      if (!r.ok) {
-        return;
-      }
-      return r.json();
-    })
-    .then(data => {
-      page += 1;
-      return renderSearchCard(data.hits);
-    })
-    .catch(console.log);
+  page += 1;
+  getImgs(searchValue);
 }
 
-function renderSearchCard(datas) {
-  const cards = datas
+function renderSearchCard(data) {
+  const cards = data
     .map(
-      data => `<div class="photo-card">
-  <img src="${data.webformatURL}" alt="${data.tags}" loading="lazy" />
+      datum => `<div class="photo-card">
+      <a class="gallery__link" href="${datum.largeImageURL}">
+  <img src="${datum.webformatURL}" alt="${datum.tags}" loading="lazy" />
+  </a>
   <div class="info">
     <p class="info-item">
-      <b>Likes: <span>${data.likes}</span></b>
+      <b>Likes: <span>${datum.likes}</span></b>
     </p>
     <p class="info-item">
-      <b>Views: <span>${data.views}</span></b>
+      <b>Views: <span>${datum.views}</span></b>
     </p>
     <p class="info-item">
-      <b>Comments: <span>${data.comments}</span></b>
+      <b>Comments: <span>${datum.comments}</span></b>
     </p>
     <p class="info-item">
-      <b>Downloads: <span>${data.downloads}</span></b>
+      <b>Downloads: <span>${datum.downloads}</span></b>
     </p>
   </div>
 </div>`
     )
     .join('');
-  refs.gallery.innerHTML = cards;
+
+  refs.gallery.innerHTML += cards;
+}
+
+function openModal() {
+  const lightbox = new SimpleLightbox('.photo-card a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lightbox.refresh();
 }
